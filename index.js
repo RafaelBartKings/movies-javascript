3e29b73541c3654288f4966671b87459e84d80c3
Rafael Reis <rafyangtj@gmail.com>
2024-01-01 11:16:52 -0300

    added movieTemplate

--- index.js
+++ index.js
@@ -51,7 +51,9 @@ const onInput = async event => {
     option.addEventListener('click', () => {
       dropdown.classList.remove('is-active');
       input.value = movie.Title;
-    })
+      onMovieSelect(movie)
+
+    });
 
     resultsWrapper.appendChild(option);
   }
@@ -65,3 +67,32 @@ document.addEventListener('click', event => {
   }
 });
 
+const onMovieSelect = async (movie) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apiKey: 71922265,
+      i: movie.imdbID
+    }
+  });
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+};
+
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+  `
+};
